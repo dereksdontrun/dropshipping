dfde955080c062dd3fdfd4b27ab99336158fefb7
dereksdontrun <dereksdontrun87@gmail.com>
2022-02-17 18:25:11 +0100

    2 - Función que chequea cambios de estado de pedido, en caso de estado correcto, chequeamos si contiene productos dropshipping

--- dropshipping.php
+++ dropshipping.php
@@ -281,6 +281,121 @@ class Dropshipping extends Module
         // );
     public function hookActionOrderStatusPostUpdate($params)
     {
-        
+        //vamos a comprobar cada cambio de estado, pero solo nos interesan los que van a Verificando stock, antiguo Sin Stock Pagado, ya que si entran en estado no pagado no queremos hacer todavía el pedido y si son pago aceptado es que tenemos stock y no hay que pedirlo
+        if ($params) {
+            $new_order_status = $params['newOrderStatus'];
+            if (Validate::isLoadedObject($new_order_status) && $new_order_status->id == Configuration::get(PS_OS_OUTOFSTOCK_PAID)){ 
+                // $estado_pedido = $new_order_status->id;
+                $id_order = $params['id_order'];
+
+                //chequeamos si ya ha pasado por aquí el pedido
+                if ($this->checkPedidoProcesado($id_order)) {
+                    return;
+                }
+
+                //chequeamos si algún producto corresponde a proveedor dropshipping. Devuelve false si no hay productos dropshipping y un array con los productos y sus proveedores si hay uno o varios
+                if (!$info_dropshipping = $this->checkProveedorDropshipping($id_order)) {
+                    return;
+                }
+
+                //si hay productos sin stock de proveedores dropshipping los tenemos en $info_dropshipping
+
+                    
+                
+
+
+            } //if validate status and status verificando stock
+
+        } // if $params
+
+    }
+
+    //función que comprueba si el pedido ya existe en la tabla de pedidos dropshipping (en caso de ser un cambio de estado manual, porque si se cambia el estado manualmente una vez ha entrado se duplicaría la entrada), también deberá comprobar, si existe, si los productos dropshipping actuales son los mismos y actuar en consecuencia. De momento simplemente, si lo encuentra pasamos del pedido    
+    public function checkPedidoProcesado($id_order) 
+    {
+        //por ahora solo comprobamos si existe - habrá que añadir que compruebe si los productos son los mismos en el futuro
+        $sql_existe_pedido = 'SELECT id_dropshipping_pedidos
+        FROM lafrips_dropshipping_pedidos                     
+        WHERE id_order = '.$id_order;
+
+        if (Db::getInstance()->ExecuteS($sql_existe_pedido)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    //obtenemos los productos del pedido y comprobamos si alguno corresponde a proveedor dropshipping y si por su stock(falta de) hay que pedirlo al proveedor. Devuelve false si no encuentra productos para dropshipping y un array con lso productos y sus respectivos proveedores, etc si los encuentra
+    //Si hay productos dropshipping devuelve un array multidimensional. en $info_dropshipping[0] irá la información del pedido (id_customer, id_address_delivery, etc) y luego irá otro subarray por proveedor dropshipping. Si hay de disfrazzes será $info_dropshipping[id_proveedor_disfrazzes], y dentro tantos arrays como productos dropshipping de dicho array con su info. Si luego hubiera un producto dmi, $info_dropshipping[id_proveedor_dmi]
+    public function checkProveedorDropshipping($id_order) 
+    {
+        $proveedores_dropshipping = explode(",", Configuration::get(PROVEEDORES_DROPSHIPPING));
+        $num_dropshipping = 0;
+        $num_no_dropshipping = 0;
+        $info_dropshipping = array();
+
+        $order = new Order($id_order);
+        if (Validate::isLoadedObject($order)){ 
+            //sacamos los productos del pedido                   
+            $order_products = $order->getProducts();      
+
+            //comprobamos si hay algún producto dropshipping en el pedido, y su stock en order_detail 
+            if ($order_products) {
+                foreach ($order_products as $producto) {
+                    if (in_array((int)$producto['id_supplier'], $proveedores_dropshipping)) {                     
+                        $producto_dropshipping = array();
+                        //si el producto no está en gestión avanzada, pasamos al siguiente
+                        if (!StockAvailableCore::dependsOnStock($producto['product_id'])){
+                            $num_no_dropshipping++;
+                            continue;
+                        }      
+                        
+                        //product_quantity_in_stock indica las unidades disponibles en el momento de la compra sobre las compradas, es decir, si se compran 5 y hay 7, valdrá 5. Si se compran 5 y hay 3 valdrá 3. Salvo para pedidos de worten y amazon que su módulo tiene algo mal y product_quantity_in_stock equivale al stock total en el momento de compra (creo que disponible)
+                        //
+                        //si la cantidad en stock es mayor o igual que las unidades compradas, no es sin stock, pasamos al siguiente.                         
+                        if ($producto['product_quantity_in_stock'] >= $producto['product_quantity']) {
+                            $num_no_dropshipping++;
+                            continue;
+                        }   
+                        
+                        $num_dropshipping++;
+                        
+                        //en este punto, se ha vendido un producto sin stock de proveedor dropshipping   
+                        $producto_dropshipping['id_order_detail'] = $producto['id_order_detail'];                                                  
+                        $producto_dropshipping['id_supplier'] = (int)$producto['id_supplier'];
+                        $producto_dropshipping['id_product'] = $producto['product_id']; 
+                        $producto_dropshipping['id_product_attribute'] = $producto['product_attribute_id'];
+                        $producto_dropshipping['product_name'] = pSQL($producto['product_name']);
+                        $producto_dropshipping['product_reference'] = pSQL($producto['product_reference']);
+                        $producto_dropshipping['product_supplier_reference'] = pSQL($producto['product_supplier_reference']);
+                        $producto_dropshipping['product_quantity'] = $producto['product_quantity']; 
+
+                        $info_dropshipping[(int)$producto['id_supplier']][] = $producto_dropshipping;
+
+                    } else {
+                        //este producto no tiene supplier dropshipping
+                        $num_no_dropshipping++;
+                        continue;
+                    }                                 
+                }                                
+            }
+        }
+
+        if ($num_dropshipping) {
+            //hay dropshipping, sacamos información sobre el pedido y la metemos en la posición 0 del array info
+            //si en info_dropshipping hay más un array es que hay más de un proveedor, contamos antes de meter info de pedido
+            $numero_proveedores = count($info_dropshipping);
+            $info_dropshipping[0]['id_order'] = $id_order;
+            $info_dropshipping[0]['proveedores_dropshipping'] = $numero_proveedores;                      
+            $info_dropshipping[0]['otros_productos'] = $num_no_dropshipping; //si hay productos no dropshipping  
+            $info_dropshipping[0]['total_productos'] = $num_dropshipping + $num_no_dropshipping;
+            $info_dropshipping[0]['id_customer'] = $order->id_customer;  
+            $info_dropshipping[0]['id_address_delivery'] = $order->id_address_delivery; 
+
+            return $info_dropshipping;
+        }
+
+        return false;
+
     }
 }
